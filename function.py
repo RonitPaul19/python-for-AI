b3ffb8965b357d263d4b8e27d77be367ce26d1e6
RonitPaul19 <ronitpaulindia@gmail.com>
2026-06-17 17:26:12 +0530

    Initial commit

---
+++ function.py
@@ -0,0 +1,9 @@
+def ADD(a,b):
+  return a+b
+
+def main():
+  a = int(input("Enter first number : "))
+  b = int(input("Enter first number : "))
+  print(ADD(a,b))
+
+main()
---
+++ import.py
@@ -0,0 +1,31 @@
+import math  # import the whole module
+
+print(math.sqrt(9))  # 3.0
+
+# from math import sqrt
+# print(sqrt(9)) # 3.0
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+import random
+
+number = random.randint(1, 10)
+choice = random.choice(["Apple", "Banana", "Orange"])
+
+print(number)
+print(choice)
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+import datetime
+
+today = datetime.date.today()
+print(today)
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+import os
+current_dir = os.getcwd()
+print(current_dir)
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = =
